a6125f1dfb5ecf44210a519959bf8010c9171182
Caroline Longnecker <cvrieous@gmail.com>
2019-12-28 00:43:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,9 @@
 class TicTacToe
   def initialize
-    @board = [" ", " ", " ", 
-      " ", " ", " ", 
-      " ", " ", " "]
+    @board = 
+      [" ", " ", " ", 
+       " ", " ", " ", 
+       " ", " ", " "]
       
   end
 end
